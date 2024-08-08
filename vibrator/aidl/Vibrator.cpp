0d8c87dd40f4c1ebd56a4f94dc3514a4236fdef8
AdarshGrewal <adarshgrewal@gmail.com>
2024-08-08 13:09:07 +0530

    vibrator: Add soc-id for pineapple
    Change-Id: Ie4cd6f360e976fa68caf4e864483d9a85e71d042
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- vibrator/aidl/Vibrator.cpp
+++ vibrator/aidl/Vibrator.cpp
@@ -76,6 +76,7 @@ namespace vibrator {
 #define MSM_CPU_CAPE            530
 #define APQ_CPU_CAPE            531
 #define MSM_CPU_KALAMA          519
+#define MSM_CPU_PINEAPPLE       614
 
 #define test_bit(bit, array)    ((array)[(bit)/8] & (1<<((bit)%8)))
 
