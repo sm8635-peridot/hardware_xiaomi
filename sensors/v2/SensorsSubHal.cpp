f7b88be5072c0fe7477719467d98b0bb953f1a7e
kenway214 <kenway214@outlook.com>
2025-03-19 22:23:01 +0530

    Revert "sensors: Implement udfps long press sensor"
    This reverts commit 99034bd57c256974a0f920b39b20a1b3228d9372.

--- sensors/v2/SensorsSubHal.cpp
+++ sensors/v2/SensorsSubHal.cpp
@@ -17,7 +17,6 @@
 #include "SensorsSubHal.h"
 
 #include <android/hardware/sensors/2.1/types.h>
-#include <cutils/properties.h>
 #include <log/log.h>
 
 using ::android::hardware::sensors::V2_1::implementation::ISensorsSubHal;
@@ -33,11 +32,7 @@ namespace implementation {
 using ::android::hardware::Void;
 using ::android::hardware::sensors::V2_0::implementation::ScopedWakelock;
 
-SensorsSubHal::SensorsSubHal() : mCallback(nullptr), mNextHandle(1) {
-    if (property_get_bool("ro.vendor.sensors.xiaomi.udfps", false)) {
-        AddSensor<UdfpsSensor>();
-    }
-}
+SensorsSubHal::SensorsSubHal() : mCallback(nullptr), mNextHandle(1) {}
 
 Return<void> SensorsSubHal::getSensorsList_2_1(ISensors::getSensorsList_2_1_cb _hidl_cb) {
     std::vector<SensorInfo> sensors;
